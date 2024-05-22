217272286f5742dad77f08d02bf1091e1374d9d0
Malehe3 <160879306+Malehe3@users.noreply.github.com>
2024-05-22 14:49:49 -0500

    Update appy.py

--- appy.py
+++ appy.py
@@ -1,5 +1,25 @@
 import streamlit as st
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
+
+# Importar la biblioteca para reconocimiento de voz
+import speech_recognition as sr
+
+# Función para tomar la foto
+def take_photo():
+    img_file_buffer = st.camera_input("Toma una Foto")
+    if img_file_buffer is not None:
+        image = Image.open(img_file_buffer)
+        st.image(image, caption="Tu Señal de Identificación")
+        image.save("señal_identificacion.jpg")
+        st.download_button(
+            label="Descargar",
+            data=open("señal_identificacion.jpg", "rb").read(),
+            file_name="señal_identificacion.jpg",
+            mime="image/jpeg" 
+        )
+
+# Configurar el reconocedor de voz
+r = sr.Recognizer()
 
 st.title("¡Aprende Lenguaje de Señas Colombiano!")
 
@@ -24,22 +44,18 @@ st.write("""
 Captura una característica distintiva, ya sea física, de personalidad o relacionada con una experiencia memorable y crea tu propia seña:
 """)
 
-voice_command = st.text_input("Diga 'foto' para tomar una foto")
-
-if voice_command.lower() == "foto":
-    img_file_buffer = st.camera_input("Toma una Foto")
-
-    if img_file_buffer is not None:
-        image = Image.open(img_file_buffer)
-        st.image(image, caption="Tu Señal de Identificación")
-        
-        image.save("señal_identificacion.jpg")
-        st.download_button(
-            label="Descargar",
-            data=open("señal_identificacion.jpg", "rb").read(),
-            file_name="señal_identificacion.jpg",
-            mime="image/jpeg" 
-        )
+# Esperar el comando de voz del usuario
+with sr.Microphone() as source:
+    st.write("Diga 'foto' para tomar una foto")
+    audio_data = r.record(source, duration=5)  # Escuchar durante 5 segundos
+    try:
+        text = r.recognize_google(audio_data, language="es-ES")
+        if "foto" in text.lower():
+            take_photo()
+    except sr.UnknownValueError:
+        st.write("No se pudo entender el comando de voz")
+    except sr.RequestError as e:
+        st.write(f"Error al realizar la solicitud al servicio de reconocimiento de voz; {e}")
 
 st.write("""
 ### ¡Comparte tu Señal!
